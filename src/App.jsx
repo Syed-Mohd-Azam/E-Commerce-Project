f79e5c49af03f43949808c46260ecdf376d0767c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-05 23:25:01 +0530

    Header and Footer Components created and provided to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import AllRoutes from "./routes/AllRoutes";
+import { Header, Footer } from "./components";
 function App() {
   return (
     <>
       <section>
+        <Header />
         <AllRoutes />
+        <Footer />
       </section>
     </>
   );
