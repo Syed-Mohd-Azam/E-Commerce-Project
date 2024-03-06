297030defefa15f219861775067246098002c4db
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-06 17:43:48 +0530

    Header Component Styling Added

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,7 +1,39 @@
 export const Header = () => {
   return (
     <>
-      <header>Header</header>
+      <header>
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+          <div className="flex flex-wrap justify-between items-center mx-auto md:w-4/5 w-full p-4">
+            <a
+              href="https://flowbite.com"
+              className="flex items-center space-x-3 rtl:space-x-reverse"
+            >
+              <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="md:h-8 h-5"
+                alt="Flowbite Logo"
+              />
+              <span className="self-center text-xl md:text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Flowbite
+              </span>
+            </a>
+            <div className="flex items-center space-x-6 rtl:space-x-reverse">
+              <a
+                href="tel:5541251234"
+                className="text-sm  text-gray-500 dark:text-white hover:underline"
+              >
+                (555) 412-1234
+              </a>
+              <a
+                href="#"
+                className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
+              >
+                Login
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
     </>
   );
 };
