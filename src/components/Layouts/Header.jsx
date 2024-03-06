ed54a6017e0bbcebfc08d3e927c27c6d96bd525f
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-06 18:27:19 +0530

    bootstrap-icons  package uninstalled

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -18,20 +18,7 @@ export const Header = () => {
                 Flowbite
               </span>
             </a>
-            <div className="flex items-center space-x-6 rtl:space-x-reverse">
-              <a
-                href="tel:5541251234"
-                className="text-sm  text-gray-500 dark:text-white hover:underline"
-              >
-                (555) 412-1234
-              </a>
-              <a
-                href="#"
-                className="text-sm  text-blue-600 dark:text-blue-500 hover:underline"
-              >
-                Login
-              </a>
-            </div>
+            <div className="flex items-center space-x-6 rtl:space-x-reverse"></div>
           </div>
         </nav>
       </header>
