bce95dade851c5d52af65218f1be7b75a0be918d
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-06 20:30:05 +0530

    Changes Added

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -18,15 +18,15 @@ export const Header = () => {
                 className="md:h-8 h-5"
                 alt="Flowbite Logo"
               />
-              <span className="self-center text-xl md:text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-blue-900  text-xl md:text-2xl font-bold whitespace-nowrap dark:text-white">
                 CodeBook
               </span>
             </Link>
             <div className="flex items-center md:space-x-5 space-x-3 rtl:space-x-reverse ">
-              <GrSettingsOption className="md:w-5 md:h-5 h-4 w-4 dark:text-white cursor-pointer" />
-              <IoSearch className="md:w-5 md:h-5 h-4 w-4 dark:text-white cursor-pointer" />
-              <FaShoppingCart className="md:w-5 md:h-5 h-4 w-4 dark:text-white cursor-pointer" />
-              <GoPerson className="md:w-5 md:h-5 h-4 w-4 dark:text-white cursor-pointer" />
+              <GrSettingsOption className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
+              <IoSearch className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
+              <FaShoppingCart className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
+              <GoPerson className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
             </div>
           </div>
         </nav>
