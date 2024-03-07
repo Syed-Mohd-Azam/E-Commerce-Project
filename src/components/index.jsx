18ff4b93cf58dca1242e730ffdfe73c3f3017c1b
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-07 23:58:53 +0530

    Changes Done

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { Header } from "./Layouts/Header";
 export { Footer } from "./Layouts/Footer";
+// eslint-disable-next-line react-refresh/only-export-components
 export { scrollToTop } from "./other/ScrollToTop";
