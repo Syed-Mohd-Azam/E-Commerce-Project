cbef0ff22e7059ab76ffa05c94ca0a528527dd34
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-08 18:40:11 +0530

    Changes Done

--- src/components/Layouts/Footer.jsx
+++ src/components/Layouts/Footer.jsx
@@ -4,10 +4,10 @@ import { FaGithub } from "react-icons/fa";
 export const Footer = () => {
   return (
     <>
-      <footer>
+      <footer className="border-t-2 border-gray-200">
         <footer className="bg-white shadow-none dark:bg-blue-900">
           <div className=" mx-auto md:w-4/5 w-full flex items-center justify-between p-3">
-            <p className="text-sm sm:text-lg text-blue-900 font-bold  dark:text-white italic text-center">
+            <p className="text-sm sm:text-lg text-blue-900 font-bold  dark:text-gray-200 italic text-center">
               © 2024 & All Rights Reserved.
             </p>
             <div className="flex space-x-6 justify-center mt-0">
@@ -15,10 +15,10 @@ export const Footer = () => {
                 to="https://www.linkedin.com/in/syed-mohd-azam/"
                 target="_blank"
               >
-                <FaLinkedin className="w-4 h-4 sm:w-6 sm:h-6 text-blue-900 dark:text-white" />
+                <FaLinkedin className="w-4 h-4 sm:w-6 sm:h-6 text-blue-900 dark:text-gray-200" />
               </Link>
               <Link to="https://github.com/Syed-Mohd-Azam" target="_blank">
-                <FaGithub className="w-4 h-4 sm:w-6 sm:h-6 text-blue-900 dark:text-white" />
+                <FaGithub className="w-4 h-4 sm:w-6 sm:h-6 text-blue-900 dark:text-gray-200" />
               </Link>
             </div>
           </div>
--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -6,8 +6,8 @@ import { GoPerson } from "react-icons/go";
 export const Header = () => {
   return (
     <>
-      <header className="shadow-none">
-        <nav className="bg-white border-gray-200 dark:bg-blue-900">
+      <header className="border-b-2  dark:border-gray-200 border-black">
+        <nav className="bg-white border-gray-200  dark:bg-blue-900 ">
           <div className="flex flex-wrap justify-between items-center mx-auto md:w-4/5 w-full p-4">
             <Link
               to="/"
@@ -18,20 +18,20 @@ export const Header = () => {
                 className="md:h-8 h-5"
                 alt="Flowbite Logo"
               />
-              <span className="self-center text-blue-900  text-xl md:text-2xl font-bold whitespace-nowrap dark:text-white">
+              <span className="self-center text-blue-900  text-xl md:text-2xl font-bold whitespace-nowrap dark:text-gray-200">
                 CodeBook
               </span>
             </Link>
             <div className="flex items-center md:space-x-5 space-x-3 rtl:space-x-reverse ">
-              <GrSettingsOption className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
-              <IoSearch className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
+              <GrSettingsOption className="md:w-6 md:h-6 h-4 w-4 dark:text-gray-200 cursor-pointer text-blue-900" />
+              <IoSearch className="md:w-6 md:h-6 h-4 w-4 dark:text-gray-200 cursor-pointer text-blue-900" />
               <div className="flex flex-col relative">
-                <FaShoppingCart className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
-                <span className="absolute -right-1 -top-3 bg-rose-700 text-white px-1 font-bold rounded-full text-xs">
+                <FaShoppingCart className="md:w-6 md:h-6 h-4 w-4 dark:text-gray-200 cursor-pointer text-blue-900" />
+                <span className="absolute -right-1 -top-3 bg-rose-700 text-gray-200 px-1 font-bold rounded-full text-xs">
                   0
                 </span>
               </div>
-              <GoPerson className="md:w-6 md:h-6 h-4 w-4 dark:text-white cursor-pointer text-blue-900" />
+              <GoPerson className="md:w-6 md:h-6 h-4 w-4 dark:text-gray-200 cursor-pointer text-blue-900" />
             </div>
           </div>
         </nav>
