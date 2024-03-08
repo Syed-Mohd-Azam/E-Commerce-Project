811026862da31c472a4774a0067e9da42ceefe01
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-08 20:46:43 +0530

    Frequently Asked Questions Component Completed

--- src/pages/Home/components/Accordian.jsx
+++ src/pages/Home/components/Accordian.jsx
@@ -1,15 +1,30 @@
 /* eslint-disable react/prop-types */
-export const Accordian = ({ question, answer }) => {
-  console.log(question, answer);
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosArrowUp } from "react-icons/io";
+export const Accordian = ({ question, answer, index, setIndex, id }) => {
   return (
     <>
-      <section className="py-3 px-2">
-        <article>
-          <p className="font-semibold text-xl">{question}</p>
+      <section className="py-5 px-4 bg-purple-50 mb-4 rounded-xl dark:bg-gray-300">
+        <article className="flex justify-between">
+          <p className="font-semibold text-xl dark:text-blue-900">{question}</p>
+          {!index && (
+            <IoIosArrowUp
+              onClick={() => setIndex(id)}
+              className="hover:cursor-pointer dark:text-blue-900 font-bold w-6 h-6"
+            />
+          )}
+          {index && (
+            <IoIosArrowDown
+              onClick={() => setIndex(0)}
+              className="hover:cursor-pointer dark:text-blue-900 font-bold w-6 h-6"
+            />
+          )}
         </article>
-        <article className="py-5 px-4 font-normal italic text-lg">
-          {answer}
-        </article>
+        {index && (
+          <article className="py-5 px-4 font-semibold italic text-lg dark:text-blue-900">
+            {answer}
+          </article>
+        )}
       </section>
     </>
   );
--- src/pages/Home/components/FaqComponent.jsx
+++ src/pages/Home/components/FaqComponent.jsx
@@ -1,16 +1,25 @@
 import { Faq } from "../../../utils/Faq";
 import { Accordian } from "./Accordian";
+import { useState } from "react";
 export const FaqComponent = () => {
+  const [index, setIndex] = useState(0);
   return (
     <>
       <section className="dark:bg-blue-900 bg-white">
         <article className="w-full lg:w-4/5 mx-auto py-10 px-4 lg:px-0">
-          <p className="lg:text-3xl  text-2xl font-bold text-center pt-20 pb-20 italic">
+          <p className="lg:text-3xl  text-2xl font-bold text-center pt-20 pb-20 italic dark:text-gray-200">
             Frequently Asked Questions !!
           </p>
           {Faq?.map(({ id, question, answer }) => (
             <>
-              <Accordian key={id} question={question} answer={answer} />
+              <Accordian
+                key={id}
+                id={id}
+                question={question}
+                answer={answer}
+                index={index === id ? true : false}
+                setIndex={setIndex}
+              />
             </>
           ))}
         </article>
