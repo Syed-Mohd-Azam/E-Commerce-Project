1158a96705b6e22a5e733998d423664af121e2da
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-08 17:34:19 +0530

    Hero Section Completed

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,7 +2,7 @@ import { Hero } from "./components/Hero";
 export const HomePage = () => {
   return (
     <>
-      <section className="bg-white dark:bg-blue-900">
+      <section className="bg-white dark:bg-blue-900 w-full lg:w-4/5 mx-auto">
         <Hero />
       </section>
     </>
