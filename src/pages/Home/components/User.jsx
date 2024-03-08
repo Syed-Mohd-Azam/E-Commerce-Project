8930a442d4485c3898c85757fead890649ad2f59
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-09 00:37:19 +0530

    Testimonials Component Completed

--- src/pages/Home/components/User.jsx
+++ src/pages/Home/components/User.jsx
@@ -3,17 +3,17 @@
 export const User = ({ user: { img, review, position, company, name } }) => {
   return (
     <>
-      <section className="md:basis-2/5 p-4 w-4/5 border-2 rounded-lg ">
+      <section className="md:basis-2/5 p-4 w-10/12 border-2 rounded-lg ">
         <blockquote className="italic font-normal lg:text-lg">
           {"' "}
           {review}
           {" '"}
         </blockquote>
-        <article className="flex lg:flex-row flex-col lg:gap-10 gap-3 lg:py-8 py-2 px-5">
-          <img src={img} alt="Image" className="w-20 h-20 rounded-full" />
-          <article className="flex flex-col gap-2">
+        <article className="flex sm:flex-row flex-col lg:gap-10 gap-5 lg:py-8 py-2 px-5 ">
+          <img src={img} alt="Image" className="w-24 h-24 rounded-full" />
+          <article className="flex flex-col gap-2 ">
             <p className="font-bold italic">{name}</p>
-            <p className="font-bold italic">
+            <p className="font-bold italic ">
               {position} at {company}
             </p>
           </article>
