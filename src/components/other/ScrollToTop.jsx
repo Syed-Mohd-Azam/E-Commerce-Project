f50bd061ffd69bb6623cf404bf516b8ebc913ba3
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-12 22:11:32 +0530

    Changes Done

--- src/components/other/ScrollToTop.jsx
+++ src/components/other/ScrollToTop.jsx
@@ -1,9 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 export const ScrollToTop = () => {
-  console.log("scroll to top called!");
   const { pathname } = useLocation();
-  console.log(pathname);
   useEffect(() => {
     window.scroll(0, 0);
   }, [pathname]);
