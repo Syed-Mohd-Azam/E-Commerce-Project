69327a26a1a58ca01862fe947e343bf7f5dc5ef8
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-12 20:23:30 +0530

    Changes Added

--- src/pages/Home/components/Hero.jsx
+++ src/pages/Home/components/Hero.jsx
@@ -15,7 +15,7 @@ export const Hero = () => {
         <Link
           to="/products"
           type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-gray-200 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 dark:text-blue-900 tracking-wider font-bold"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg text-lg px-5 py-2.5 mr-2 mb-2 dark:bg-gray-200 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 dark:text-slate-800 dark:hover:text-purple-50 tracking-wider font-bold"
         >
           Explore eBooks
         </Link>
