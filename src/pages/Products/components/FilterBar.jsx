d820d0cca58b5fa37a8506963e95b8399d10ef58
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-13 20:42:51 +0530

    Best Seller Only Filter Added

--- src/pages/Products/components/FilterBar.jsx
+++ src/pages/Products/components/FilterBar.jsx
@@ -2,16 +2,16 @@ import { IoMdClose } from "react-icons/io";
 export const FilterBar = () => {
   return (
     <>
-      <section className="h-screen fixed top-0 left-0 shadow w-[200px] z-10 bg-white dark:bg-slate-800 p-3">
+      <section className="h-screen fixed top-0 left-0 shadow w-[230px] z-10 bg-white dark:bg-slate-800 p-3">
         <article className="flex justify-between  items-center">
-          <p className=" text-blue-900 dark:text-gray-200 font-bold text-lg">
+          <p className=" text-blue-900 dark:text-gray-200 font-bold text-2xl italic">
             Filter
           </p>
-          <IoMdClose className="w-6 h-6 text-blue-900 dark:text-gray-200" />
+          <IoMdClose className="w-8 h-8 text-blue-900 dark:text-gray-200 hover:bg-gray-200 rounded-full p-1 hover:cursor-pointer dark:hover:bg-slate-500 " />
         </article>
         <hr className="mt-2 text-blue-900 dark:text-gary-200" />
         <article className="py-4 text-blue-900 dark:text-gray-200">
-          <p className="font-bold pb-2">Sort By</p>
+          <p className="font-bold pb-4 text-lg text-start px-4">Sort By</p>
           <div className="flex items-center justify-center flex-row gap-2">
             <input type="radio" id="sort-by-1" name="sort-by" value="" />
             <label htmlFor="sort-by-1">Price - Low to High</label>
@@ -20,6 +20,63 @@ export const FilterBar = () => {
             <input type="radio" id="sort-by-2" name="sort-by" value="" />
             <label htmlFor="sort-by-2">Price - High to Low</label>
           </div>
+        </article>
+        <article>
+          <p className="font-bold text-blue-900 dark:text-gray-200 text-lg text-start pb-4 px-4">
+            Rating
+          </p>
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <input type="radio" id="ratings-1" name="ratings" value="" />
+            <label
+              htmlFor="ratings-1"
+              className=" text-blue-900 dark:text-gray-200"
+            >
+              4 Stars & Above
+            </label>
+          </div>
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <input type="radio" id="ratings-2" name="ratings" value="" />
+            <label
+              htmlFor="ratings-2 "
+              className="text-blue-900 dark:text-gray-200"
+            >
+              3 Stars & Above
+            </label>
+          </div>
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <input type="radio" id="ratings-3" name="ratings" value="" />
+            <label
+              htmlFor="ratings-3 "
+              className="text-blue-900 dark:text-gray-200"
+            >
+              2 Stars & Above
+            </label>
+          </div>
+          <div className="flex flex-row gap-4 items-center justify-center">
+            <input type="radio" id="ratings-4" name="ratings" value="" />
+            <label
+              htmlFor="ratings-4 "
+              className="text-blue-900 dark:text-gray-200"
+            >
+              1 Stars & Above
+            </label>
+          </div>
+          <article>
+            <p className="font-bold text-blue-900 dark:text-gray-200 px-4 py-4">
+              Other Filters
+            </p>
+            <div className="flex flex-row justify-center items-center gap-4">
+              <input type="checkbox" id="seller" name="best-sellers" value="" />
+              <label
+                htmlFor="seller"
+                className="text-blue-900 dark:text-gray-200"
+              >
+                {" "}
+                Best Seller Only
+              </label>
+            </div>
+            <div></div>
+          </article>
         </article>
       </section>
     </>
