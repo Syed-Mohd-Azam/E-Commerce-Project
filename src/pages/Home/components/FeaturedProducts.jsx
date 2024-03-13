b3863b5ac11d1b33b93d16133ed117b8f06d3f84
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-13 15:37:51 +0530

    Changes Added

--- src/pages/Home/components/FeaturedProducts.jsx
+++ src/pages/Home/components/FeaturedProducts.jsx
@@ -3,10 +3,10 @@ export const FeaturedProducts = () => {
   return (
     <>
       <section className="w-full lg:w-4/5 mx-auto ">
-        <h1 className="lg:text-3xl italic text-blue-900 font-bold text-center lg:py-16 text-2xl py-4">
+        <h1 className="lg:text-3xl italic text-blue-900 font-bold text-center lg:py-16 text-2xl py-4 dark:text-gray-200">
           Featured E-Books!
         </h1>
-        <article>
+        <article className="flex flex-row gap-5">
           <ProductCard />
           <ProductCard />
           <ProductCard />
--- src/pages/Home/components/ProductCard.jsx
+++ src/pages/Home/components/ProductCard.jsx
@@ -1,7 +1,24 @@
 export const ProductCard = () => {
   return (
     <>
-      <section></section>
+      <section className="border-2 border-gray-200 h-[350px] flex-1 rounded-xl">
+        <article className="w-full h-1/2 ">
+          <img
+            src=""
+            alt="Book-Image"
+            className="w-full h-full dark:text-gray-200"
+          />
+        </article>
+        <article>
+          <p>Title</p>
+          <p>desription of book</p>
+          <article className="flex flex-row gap-2"></article>
+          <article className="flex justify-between items-center">
+            <p></p>
+            <button>Add to cart</button>
+          </article>
+        </article>
+      </section>
     </>
   );
 };
