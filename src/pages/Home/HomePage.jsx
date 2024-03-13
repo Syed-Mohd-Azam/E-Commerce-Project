1f46e96bb6ed1c6a5640ba5475cdb25235ee36ac
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-03-13 15:21:24 +0530

    ProductCard Component Created

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,11 +1,13 @@
 import { Hero } from "./components/Hero";
 import { FaqComponent } from "./components/FaqComponent";
 import Testimonials from "./components/Testimonials";
+import { FeaturedProducts } from "./components/FeaturedProducts";
 export const HomePage = () => {
   return (
     <>
       <section className="bg-white dark:bg-slate-800 w-full ">
         <Hero />
+        <FeaturedProducts />
         <Testimonials />
         <FaqComponent />
       </section>
--- src/pages/Home/components/FeaturedProducts.jsx
+++ src/pages/Home/components/FeaturedProducts.jsx
@@ -1,7 +1,17 @@
+import { ProductCard } from "./ProductCard";
 export const FeaturedProducts = () => {
   return (
     <>
-      <section></section>
+      <section className="w-full lg:w-4/5 mx-auto ">
+        <h1 className="lg:text-3xl italic text-blue-900 font-bold text-center lg:py-16 text-2xl py-4">
+          Featured E-Books!
+        </h1>
+        <article>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </article>
+      </section>
     </>
   );
 };
