cc53f14a9759a362be8e016642f68859f60972e7
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-04-27 01:56:28 +0530

    Changes Added

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -23,17 +23,17 @@ export const ProductDetails = () => {
   return (
     <>
       <section className="md:w-4/5 mx-auto p-5 flex-1 ">
-        <h1 className="w-full text-center pb-8 pt-3 text-3xl text-slate-800 italic font-semibold dark:text-gray-200">
+        <h1 className="w-full text-center pb-8 pt-3 text-lg md:text-xl lg:text-3xl text-slate-800 italic font-semibold dark:text-gray-200">
           {productDetails?.name}
         </h1>
         <section className=" flex flex-col lg:flex-row gap-10">
           <img
             src={productDetails?.poster}
             alt="Book-Image"
-            className="md:h-72 "
+            className="md:h-72 h-48"
           />
           <article>
-            <p className="pb-4 text-3xl text-slate-900 font-semibold dark:text-gray-200">
+            <p className="pb-4 text-md md:text-lg lg:text-3xl text-slate-900 font-semibold dark:text-gray-200">
               {productDetails?.price}
             </p>
             <Rating rating={productDetails?.rating} />
@@ -47,7 +47,7 @@ export const ProductDetails = () => {
                 Add to Cart +
               </button>
             </article>
-            <article className="text-xl text-slate-800 italic py-4 font-normal text-justify dark:text-gray-200">
+            <article className="text-md md:text-lg lg:text-xl text-slate-800 italic py-4 font-normal text-justify dark:text-gray-200">
               {productDetails?.long_description}
             </article>
           </article>
