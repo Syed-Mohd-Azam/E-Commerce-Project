a4c415dbb80d42f9cd2b73ba67b828d9f4d8d978
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-05-05 18:26:57 +0530

    FilterContext created

---
+++ src/contexts/FilterContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext } from "react";
+
+const filterInitialState = {
+  productList: [],
+  onlyInStock: false,
+  bestSelerOnly: false,
+  sortBy: null,
+  ratings: null,
+};
+
+const FilterContext = createContext(filterInitialState);
+// eslint-disable-next-line react/prop-types
+export const FilterProvider = ({ children }) => {
+  const value = {
+    productList: [1, 2, 3],
+  };
+  return (
+    <>
+      <FilterContext.Provider value={value}>{children}</FilterContext.Provider>
+    </>
+  );
+};
+// eslint-disable-next-line react-refresh/only-export-components
+export const useFilter = () => {
+  console.log(FilterContext);
+  const context = useContext(FilterContext);
+  return context;
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { FilterProvider } from "./contexts";
 import { BrowserRouter } from "react-router-dom";
 import { ScrollToTop } from "./components/index.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ScrollToTop />
-      <App />
+      <FilterProvider>
+        <ScrollToTop />
+        <App />
+      </FilterProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
