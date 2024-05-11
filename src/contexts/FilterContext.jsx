90686465c81b044f7551149c66000e1691bb34e2
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-05-11 23:07:07 +0530

    Redux store functions added

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -20,7 +20,7 @@ export const FilterProvider = ({ children }) => {
   }
   function bestSeller(products) {
     return state?.bestSellerOnly
-      ? products.filter((product) => product.best_seller === true)
+      ? products?.filter((product) => product?.best_seller === true)
       : products;
   }
   function onlyInStock(products) {
@@ -29,31 +29,34 @@ export const FilterProvider = ({ children }) => {
       : products;
   }
   function ratings(products) {
-    if (state.ratings === "4STARSABOVE") {
-      return products.filter((product) => product.rating > 4);
+    if (state?.ratings === "4STARSABOVE") {
+      return products?.filter((product) => product?.rating > 4);
     }
-    if (state.ratings === "3STARSABOVE") {
-      return products.filter((product) => product.rating > 3);
+    if (state?.ratings === "3STARSABOVE") {
+      return products?.filter((product) => product?.rating > 3);
     }
-    if (state.ratings === "2STARSABOVE") {
-      return products.filter((product) => product.rating > 2);
+    if (state?.ratings === "2STARSABOVE") {
+      return products?.filter((product) => product?.rating > 2);
     }
-    if (state.ratings === "1STARSABOVE") {
+    if (state?.ratings === "1STARSABOVE") {
       return products.filter((product) => product.rating > 1);
     }
     return products;
   }
   function sortBy(products) {
-    if (state.sortBy === "lowtohigh") {
-      return products.sort((a, b) => Number(a.price) - Number(b.price));
+    if (state?.sortBy === "lowtohigh") {
+      return products.sort((a, b) => Number(a?.price) - Number(b?.price));
     }
-    if (state.sortBy === "hightolow") {
-      return products.sort((a, b) => Number(b.price) - Number(a.price));
+    if (state?.sortBy === "hightolow") {
+      return products.sort((a, b) => Number(b?.price) - Number(a?.price));
     }
     return products;
   }
+  const filteredProducts = ratings(
+    sortBy(onlyInStock(bestSeller(state?.productList)))
+  );
   const value = {
-    productList: state?.productList,
+    productList: filteredProducts,
     initialProductList,
   };
   return (
