e89e12869bdd8c111362034e37570ea6b01197f2
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-05-29 00:15:20 +0530

    Cart Reducer function created and used By useReducer( ) Hook

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
+import { CartReducer } from "../reducers";
 const cartInitialState = {
   cartList: [],
   total: 0,
@@ -6,11 +7,36 @@ const cartInitialState = {
 const CartContext = createContext(cartInitialState);
 // eslint-disable-next-line react/prop-types
 export const CartContextProvider = ({ children }) => {
-  console.log(CartContext);
+  const [state, dispatch] = useReducer(CartReducer, cartInitialState);
+  function addToCart(product) {
+    const updatedList = state.cartList.concat(product);
+    const total = state.total + product.price;
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: { productsList: updatedList, total: total },
+    });
+  }
+  function deleteFromCart(product) {
+    const updatedList = state.cartList.filter((item) => item.id !== product.id);
+    const total = state.total - product.price;
+    dispatch({
+      type: "REMOVE_FROM_CART",
+      payload: { productsList: updatedList, total: total },
+    });
+  }
+  function clearCart() {
+    dispatch({
+      type: "CLEAR_CART",
+    });
+  }
   const value = {
-    cartList: [],
-    total: 0,
+    cartList: state.cartList,
+    total: state.total,
+    addToCart,
+    deleteFromCart,
+    clearCart,
   };
+
   return (
     <>
       <CartContext.Provider value={value}>{children}</CartContext.Provider>
