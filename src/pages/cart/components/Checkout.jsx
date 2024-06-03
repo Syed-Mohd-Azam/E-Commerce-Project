be2a3703fbf94387858d62abd317dc70b6edd535
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-03 22:17:58 +0530

    OrderSubmit function implementation done

--- src/pages/cart/components/Checkout.jsx
+++ src/pages/cart/components/Checkout.jsx
@@ -1,15 +1,16 @@
 import { IoMdClose } from "react-icons/io";
 import { useCartContext } from "../../../contexts";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 // eslint-disable-next-line react/prop-types
 export const CheckOut = ({ setCheckOut }) => {
-  const { total } = useCartContext();
+  const { cartList, total, clearCart } = useCartContext();
+  const navigate = useNavigate();
   const [user, setUser] = useState({});
+  const token = JSON.parse(sessionStorage.getItem("token"));
+  const cbid = JSON.parse(sessionStorage.getItem("cbid"));
   useEffect(() => {
     async function getUser() {
-      const token = JSON.parse(sessionStorage.getItem("token"));
-      const cbid = JSON.parse(sessionStorage.getItem("cbid"));
       const response = await fetch(`http://localhost:8000/600/users/${cbid}`, {
         method: "GET",
         headers: {
@@ -23,6 +24,31 @@ export const CheckOut = ({ setCheckOut }) => {
     }
     getUser();
   }, []);
+  const handleOrderSubmit = async (event) => {
+    event.preventDefault();
+    const order = {
+      cartList: cartList,
+      amount: total,
+      itemsQuantity: cartList.length,
+      user: {
+        name: user.name,
+        email: user.email,
+        customerId: user.id,
+      },
+    };
+    const response = await fetch(`http://localhost:8000/660/orders`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(order),
+    });
+    const data = await response.json();
+    console.log(data);
+    clearCart();
+    navigate("/");
+  };
   return (
     <>
       <section className=" w-2/3 mx-auto  dark:bg-blue-900 shadow-lg bg-blue-400 px-10 py-5 flex-1">
@@ -37,106 +63,108 @@ export const CheckOut = ({ setCheckOut }) => {
             />
           </Link>
         </article>
-        <article>
-          <label
-            htmlFor="name"
-            className="block text-white dark:text-gray-200 mt-4 mb-2 text-xl italic font-semibold "
-          >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Enter name"
-            value={user?.name || ""}
-            required={true}
-            className="w-full rounded-md p-3 placeholder:italic placeholder:text-lg outline-0 hover:outline-0"
-          />
-        </article>
-        <article>
-          <label
-            htmlFor="email"
-            className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            placeholder="example@gmail.com"
-            id="email"
-            value={user?.email || ""}
-            required={true}
-            className="w-full rounded-md p-3 placeholder:italic placeholder:text-lg outline-0 hover:outline-0"
-          />
-        </article>
-        <article>
-          <label
-            htmlFor="cardNumber"
-            className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
-          >
-            Card Number
-          </label>
-          <input
-            type="number"
-            name="cardNumber"
-            id="cardNumber"
-            placeholder="Enter card number"
-            className="w-full rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic text-blue-900 hover:cursor-not-allowed cursor-not-allowed"
-            value={756823451234}
-          />
-        </article>
-        <article>
-          <label
-            htmlFor="expiryDate"
-            className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold "
-          >
-            Expiry Date
-          </label>
-          <article className="flex gap-4 outline-0 hover:outline-0">
+        <form onSubmit={(event) => handleOrderSubmit(event)}>
+          <article>
+            <label
+              htmlFor="name"
+              className="block text-white dark:text-gray-200 mt-4 mb-2 text-xl italic font-semibold "
+            >
+              Name
+            </label>
             <input
-              placeholder="Enter month"
               type="text"
-              name="month"
-              id="expiryDate"
-              value={"September"}
-              className="w-1/2 rounded-md px-4 py-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic hover:cursor-not-allowed"
-            />
-            <input
-              placeholder="Enter year"
-              type="number"
-              name="year"
-              id="expiryDate"
-              value={2027}
-              className="w-1/2 rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic text-blue-900 hover:cursor-not-allowed cursor-not-allowed"
+              name="name"
+              id="name"
+              placeholder="Enter name"
+              value={user?.name || ""}
+              required={true}
+              className="w-full rounded-md p-3 placeholder:italic placeholder:text-lg outline-0 hover:outline-0"
             />
           </article>
-        </article>
-        <article>
-          <label
-            htmlFor="securityCode"
-            className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
-          >
-            Security Code
-          </label>
-          <input
-            type="number"
-            name="securityCode"
-            id="securityCode"
-            placeholder="Enter security code"
-            className="w-full rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg text-slate-900 italic md:text-lg text-md hover:cursor-not-allowed"
-            value={574}
-          />
-        </article>
-        <p className="text-white dark:text-gray-200 text-xl md:text-3xl text-center m-8 font-bold italic  ">
-          Total : {total}$
-        </p>
-        <article className="w-full">
-          <button className="block w-full dark:text-gray-200 dark:bg-slate-800 p-3 rounded-lg text-sm  md:text-lg italic font-bold hover:cursor-pointer bg-blue-800 text-white ">
-            Proceed to Payment
-          </button>
-        </article>
+          <article>
+            <label
+              htmlFor="email"
+              className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="example@gmail.com"
+              id="email"
+              value={user?.email || ""}
+              required={true}
+              className="w-full rounded-md p-3 placeholder:italic placeholder:text-lg outline-0 hover:outline-0"
+            />
+          </article>
+          <article>
+            <label
+              htmlFor="cardNumber"
+              className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
+            >
+              Card Number
+            </label>
+            <input
+              type="number"
+              name="cardNumber"
+              id="cardNumber"
+              placeholder="Enter card number"
+              className="w-full rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic text-blue-900 hover:cursor-not-allowed cursor-not-allowed"
+              value={756823451234}
+            />
+          </article>
+          <article>
+            <label
+              htmlFor="expiryDate"
+              className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold "
+            >
+              Expiry Date
+            </label>
+            <article className="flex gap-4 outline-0 hover:outline-0">
+              <input
+                placeholder="Enter month"
+                type="text"
+                name="month"
+                id="expiryDate"
+                value={"September"}
+                className="w-1/2 rounded-md px-4 py-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic hover:cursor-not-allowed"
+              />
+              <input
+                placeholder="Enter year"
+                type="number"
+                name="year"
+                id="expiryDate"
+                value={2027}
+                className="w-1/2 rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg md:text-lg text-md italic text-blue-900 hover:cursor-not-allowed cursor-not-allowed"
+              />
+            </article>
+          </article>
+          <article>
+            <label
+              htmlFor="securityCode"
+              className="block text-white dark:text-gray-200 mt-2 mb-2 text-lg italic font-semibold"
+            >
+              Security Code
+            </label>
+            <input
+              type="number"
+              name="securityCode"
+              id="securityCode"
+              placeholder="Enter security code"
+              className="w-full rounded-md p-3 outline-0 hover:outline-0 placeholder:italic placeholder:text-lg text-slate-900 italic md:text-lg text-md hover:cursor-not-allowed"
+              value={574}
+            />
+          </article>
+          <p className="text-white dark:text-gray-200 text-xl md:text-3xl text-center m-8 font-bold italic  ">
+            Total : {total}$
+          </p>
+          <article className="w-full">
+            <button className="block w-full dark:text-gray-200 dark:bg-slate-800 p-3 rounded-lg text-sm  md:text-lg italic font-bold hover:cursor-pointer bg-blue-800 text-white ">
+              Proceed to Payment
+            </button>
+          </article>
+        </form>
       </section>
     </>
   );
