c525670b1a17cf3ecc3ef9848b738549234fd60c
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-10 18:28:57 +0530

    Changes added to the ShoppingCard Component

--- src/pages/Dashboard/components/ShoppingCard.jsx
+++ src/pages/Dashboard/components/ShoppingCard.jsx
@@ -6,17 +6,17 @@ export const ShoppingCard = ({ cart }) => {
         <article className="flex lg:flex-row gap-3 flex-col">
           <article>
             <img
-              className="w-80 h-36 "
+              className="w-80 h-36 rounded-2xl hover:scale-110 hover:cursor-pointer"
               src={cart?.image_local}
               alt="Book-Image"
             />
           </article>
           <article className="flex flex-col gap-1">
             <p className="text-blue-700 dark:text-slate-200 italic font-semibold text-md">
-              Description:{cart?.name}
+              Description : {cart?.name}
             </p>
             <p className="text-md text-blue-700 dark:text-slate-200 italic font-semibold">
-              Amount: {cart?.price}$
+              Amount : {cart?.price}$
             </p>
           </article>
         </article>
