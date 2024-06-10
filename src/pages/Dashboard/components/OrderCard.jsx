863ee868da05f7e78094449033629f2a946fb9be
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-10 18:37:56 +0530

    Changes added to OrderCart Component

--- src/pages/Dashboard/components/OrderCard.jsx
+++ src/pages/Dashboard/components/OrderCard.jsx
@@ -5,13 +5,13 @@ export const OrderCard = ({ order }) => {
   return (
     <>
       <section className="p-4 border-2 border-blue-700 dark:border-slate-200 my-4 rounded-md">
-        <article className="flex flex-row justify-between">
-          <p className="dark:text-slate-200 text-ble-700 italic text-sm md:text-lg font-semibold mb-5 text-blue-700 ">
+        <article className="flex flex-row justify-between ">
+          <span className="dark:text-black text-ble-700 italic text-sm md:text-lg font-semibold mb-5 text-black bg-yellow-300 rounded-md p-2 hover:cursor-pointer hover:scale-110">
             Order Id: {order?.id}
-          </p>
-          <p className="font-semibold italic text-blue-700 dark:text-slate-200 md:text-lg text-xs">
+          </span>
+          <button className="font-semibold italic dark:text-slate-200 md:text-lg text-xs  rounded-md bg-blue-900 px-2 text-white hover:scale-110">
             Total Amount: ${order?.amount}
-          </p>
+          </button>
         </article>
         <article>
           {order?.cartList.length > 0 ? (
