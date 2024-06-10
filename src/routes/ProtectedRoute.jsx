41ed58b0169eb0d862a0df4efc6ae54bf9689b66
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-10 13:19:22 +0530

    Changes added to the ProtectedRoute Component

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,9 +1,8 @@
 import { Navigate } from "react-router-dom";
+import { getTokenDetails } from "../services";
 // eslint-disable-next-line react/prop-types
 export const ProtectedRoute = ({ children }) => {
-  console.log(children);
-  const token = true;
+  const { token } = getTokenDetails();
   //   const token = JSON.parse(sessionStorage.getItem("token"));
-  console.log(token);
   return token ? children : <Navigate to="/login" />;
 };
